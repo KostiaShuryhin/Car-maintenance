704491d74be7c0f0833967762fe346cf12326714
Костя Шурыгин <kostiashuryhin@gmail.com>
2021-09-12 14:55:26 +0300

    Add groups fo MVC
    Add groups and files

---
+++ Car maintenance/Car maintenance/Controllers/VCSingInFlow/SignInVC.swift
@@ -0,0 +1,29 @@
+//
+//  SignInVC.swift
+//  Car maintenance
+//
+//  Created by Константин on 12.09.21.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

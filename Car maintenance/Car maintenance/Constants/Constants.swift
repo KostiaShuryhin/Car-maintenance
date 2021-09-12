fe90b59eac33d062a02ce334a1c5f1d63881c7a7
Костя Шурыгин <kostiashuryhin@gmail.com>
2021-09-12 20:34:19 +0300

    work with registration
    add logic sing in and registration
    work contyning

---
+++ Car maintenance/Car maintenance/Constants/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Car maintenance
+//
+//  Created by Константин on 12.09.21.
+//
+
+import Foundation
+
+struct Constants {
+    enum Segues {
+        static let tasks = "tasksSegue"
+    }
+}

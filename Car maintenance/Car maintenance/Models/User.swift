3d462301d9507937a2b019856b3c8a4e0478ff11
Костя Шурыгин <kostiashuryhin@gmail.com>
2021-09-13 14:39:59 +0300

    add user model

---
+++ Car maintenance/Car maintenance/Models/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  Car maintenance
+//
+//  Created by Константин on 13.09.21.
+//
+
+import Foundation
+import Firebase
+
+struct User {
+    
+    init(user: Firebase.User) {
+        self.uid = user.uid
+        self.email = user.email ?? ""
+    }
+    
+    let uid: String
+    let email: String
+    
+    
+}

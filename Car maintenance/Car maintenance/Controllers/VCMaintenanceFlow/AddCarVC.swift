fdeddac4d1a6e09df393e42c7028cc040428c4b1
Костя Шурыгин <kostiashuryhin@gmail.com>
2021-09-17 17:40:20 +0300

    Some changes may occur bug

---
+++ Car maintenance/Car maintenance/Controllers/VCMaintenanceFlow/AddCarVC.swift
@@ -0,0 +1,33 @@
+//
+//  AddCarVC.swift
+//  Car maintenance
+//
+//  Created by Константин on 17.09.21.
+//
+
+import UIKit
+
+//protocol AddCarProtocol {
+//    func SingOUT () -> ()
+//}
+//
+//extension AddCarVC: AddCarProtocol {
+//}
+
+class AddCarVC: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+////        self.tabBarController?.navigationItem.hidesBackButton = true
+//        self.tabBarController?.navigationItem.backButtonTitle = "SignOUT"
+//    }
+    
+    func SingOUT() {
+    }
+    
+}
---
+++ Car maintenance/Car maintenance/Controllers/VCMaintenanceFlow/ChooseCarVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChooseCarVC.swift
+//  Car maintenance
+//
+//  Created by Константин on 17.09.21.
+//
+
+import UIKit
+
+protocol ChooseCarVCProtocol {
+}
+
+class ChooseCarVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
